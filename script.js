6d52a77c86f72cbe4076451436b7362a2329c840
sandeep-menon <thesandeepmenon@gmail.com>
2018-07-19 17:04:42 +0200

    fixed problem on multiple click of button

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 function start(){
-    console.log('starting...................................');
-    console.log('starting ajax call.........................');
+    //console.log('starting...................................');
+    //console.log('starting ajax call.........................');
     var lsLat = "36.7201600";
     var lsLong = "-4.4203400";
     var lsDate = "today";
     $.get("https://api.sunrise-sunset.org/json?lat=" + lsLat + "&lng=" + lsLong + "&date=today").done(function(response){
         var args = arguments;
-        console.log(args);
+        //console.log(args);
         var lssunrise = args[0].results.sunrise;
         var lssunset = args[0].results.sunset;
-        $(".riseText").append(" " + lssunrise);
-        $(".setText").append(" " + lssunset);
+        $(".riseText").replaceWith("<p class = 'riseText'>Sunrise Time: " + lssunrise+"</p>");
+        $(".setText").replaceWith("<p class = 'setText'>Sunset Time: " + lssunset+"</p>");
     });
     $("#results").removeAttr("style");
-    console.log('did it work?...............................');
+    //console.log('did it work?...............................');
 }
 
 function hideData(){
+    $(".riseText").replaceWith("<p class='riseText'>Sunrise Time:</p>");
+    $(".setText").replaceWith("<p class='riseText'>Sunset Time:</p>");
     $("#results").attr("style", "display:none");
 }
