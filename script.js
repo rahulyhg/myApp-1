6bed0341b600745f35a045ab15d75f8d26d3889a
sandeep-menon <thesandeepmenon@gmail.com>
2018-07-19 15:52:46 +0200

    more functionality

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function Hello(){
-    alert('hello');
+function start(){
+    console.log('starting...................................');
+    console.log('starting ajax call.........................');
+    
+    /*$.get("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today").done(function(response){
+        console.log(arguments);
+    });*/
+    $("#results").removeAttr("style");
+    console.log('did it work?...............................');
+}
+
+function hideData(){
+    $("#results").attr("style", "display:none");
 }
