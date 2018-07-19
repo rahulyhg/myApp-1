23b97075c5fd34b9809b03da77a5e7cb5007e87b
sandeep-menon <thesandeepmenon@gmail.com>
2018-07-19 16:48:23 +0200

    more functionality

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function start(){
     console.log('starting...................................');
     console.log('starting ajax call.........................');
-    
-    /*$.get("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today").done(function(response){
-        console.log(arguments);
-    });*/
+    var lsLat = "36.7201600";
+    var lsLong = "-4.4203400";
+    var lsDate = "today";
+    $.get("https://api.sunrise-sunset.org/json?lat=" + lsLat + "&lng=" + lsLong + "&date=today").done(function(response){
+        var args = arguments;
+        console.log(args);
+        var lssunrise = args[0].results.sunrise;
+        var lssunset = args[0].results.sunset;
+        $(".riseText").append(" " + lssunrise);
+        $(".setText").append(" " + lssunset);
+    });
     $("#results").removeAttr("style");
     console.log('did it work?...............................');
 }
